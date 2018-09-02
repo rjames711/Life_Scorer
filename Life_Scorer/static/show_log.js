421086fb2bdcade5705b8eb7eb989a329f5a605a
Rob <amesrobert2@gmail.com>
2018-09-02 16:00:32 -0400

    Fixed minor issue in log filter search resulting from missing quotes in the row id

--- Life_Scorer/static/show_log.js
+++ Life_Scorer/static/show_log.js
@@ -7,8 +7,9 @@ function task_text() {
   var tasks = document.getElementsByClassName("inline buttons");
   // Loop through all list items, and hide those who don't match the search query
   for (i = 0; i < tasks.length; i++) {
-    task = tasks[i];
-      if (task.id.split('-')[0].toUpperCase().indexOf(filter) > -1) {
+    var task = tasks[i];
+    var taskName = task.id.split('-')[0].toUpperCase()
+      if (taskName.indexOf(filter) > -1) {
           task.style.display = "";
       } else {
           task.style.display = "none";
