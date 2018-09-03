e836435dd28b6883ecf5aa77bb82a3888ddd9b25
Rob <amesrobert2@gmail.com>
2018-09-02 22:52:56 -0400

    worked on search filter for log so multiple tasks can be targeted by seperating search term with a comma

--- Life_Scorer/static/show_log.js
+++ Life_Scorer/static/show_log.js
@@ -1,18 +1,29 @@
 function task_text() {
-  // Declare variables
-  var input, filter;
-  input = document.getElementById('selection');
-  filter = input.value.toUpperCase();
-
-  var tasks = document.getElementsByClassName("inline buttons");
-  // Loop through all list items, and hide those who don't match the search query
-  for (i = 0; i < tasks.length; i++) {
-    var task = tasks[i];
-    var taskName = task.id.split('-')[0].toUpperCase()
-      if (taskName.indexOf(filter) > -1) {
-          task.style.display = "";
-      } else {
-          task.style.display = "none";
-      }
+    // Declare variables
+    var input, filters;
+    input = document.getElementById('selection');
+    filters = input.value.toUpperCase();
+    filters = filters.split(",");
+  
+  
+    var tasks = document.getElementsByClassName("inline buttons");
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < tasks.length; i++) {
+      var task = tasks[i];
+      var taskName = task.id.split('-')[0].toUpperCase()
+      var hide=true;
+      for (var x in filters){
+        var filter = filters[x];
+        if (taskName.indexOf(filter) > -1) {
+            hide = false;
+        } 
+        
+        }
+        if(hide){
+                task.style.display = "none";
+        }
+        else {
+            task.style.display = "";
+    }
   }
-}
+  }
