e0edc0267b1692065c8aeddd93a53ec6fd54d0f7
Rob <amesrobert2@gmail.com>
2018-12-02 22:37:17 -0500

    Added a toggle button for the nav bar

---
+++ Life_Scorer/static/base.js
@@ -0,0 +1,9 @@
+function toggleTop(){
+    navbar = document.getElementById('nav');
+    if (nav.style.display === "none"){
+        nav.style.display = '';
+    }
+    else{
+        nav.style.display='none';
+    }
+}
