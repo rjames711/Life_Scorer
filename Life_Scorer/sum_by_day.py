efb201b383234f304d115867a5377e5a0e20310e
Rob <amesrobert2@gmail.com>
2019-06-22 08:25:51 -0400

    created and tested a function to get sums for a range of days

--- Life_Scorer/sum_by_day.py
+++ Life_Scorer/sum_by_day.py
@@ -4,6 +4,7 @@ else:
     import Life_Scorer.interface as interface
 import datetime, sqlite3, json
 
+#Gets the task somes for a particular day  (in format YYYY-MM-DD )
 def get_sums_by_day(day,user):
     day_sums = {}
     db = interface.get_task_db(user)
@@ -23,6 +24,17 @@ def get_sums_by_day(day,user):
                 day_sums[(taskname, attr)] = attributes[attr]
     return day_sums
 
+#Gets total sums for a list of days [ 'YYYY-MM-DD' , ..... ]
+def get_days_sums(days, user):
+    days_sums = {}
+    for day in days:
+        single_day_sums = get_sums_by_day(day, user)
+        for task in single_day_sums:
+            if task in days_sums:
+                days_sums[task]+=single_day_sums[task]
+            else:
+                days_sums[task] = single_day_sums[task]
+    return days_sums
 
 
 
