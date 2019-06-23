0f9ef0f23ba8c880c04a555e7a88d966b6209637
Rob <amesrobert2@gmail.com>
2019-06-23 21:02:27 +0000

    Some mods and comments on backup script

--- back_up_db.sh
+++ back_up_db.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
+#*/30 * * * * /var/www/Life_Scorer/back_up_db.sh #copy to crontab
+#Copy the contents of the db_files to backup locations
 cp -r /var/www/Life_Scorer/Life_Scorer/db_files "/var/www/Life_Scorer/backups/db_files-$(date)"
 cd /var/www/Life_Scorer/backups
-sudo ls -t | tail -n +50 | xargs -d "\n" sudo rm -r
-ls -lrt
-ls | wc -l
+#Get the last modified listings after 500 and remove them
+sudo ls -t | tail -n +500 | xargs -d "\n" sudo rm -r
 cd - 
