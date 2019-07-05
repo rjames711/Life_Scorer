2bcf580691ba89c0ab97663d654992f4b80564e1
Rob <amesrobert2@gmail.com>
2019-07-05 18:35:25 -0400

    Api wraps

--- Life_Scorer/lsapi.py
+++ Life_Scorer/lsapi.py
@@ -38,6 +38,17 @@ def tasks(user, token):
 
 
 @bp.route('/<user>/dayscore/<date>/<token>')
+@token_required
 def dayscore(user, date,token):
    score = {'score': scoring.get_day_score(date,'Rob') }
    return json.dumps(score)
+
+@bp.route('/<user>/log/<token>')
+@token_required
+def log(user,token):
+   log = interface.get_log(user)
+   #Need to get it formatted correctly for json, Feel like this is a slow and bad way to do it but it works
+   log = [list(item) for item in log] 
+   for item in log:
+      item[2] = json.loads(item[2])
+   return json.dumps(log)
