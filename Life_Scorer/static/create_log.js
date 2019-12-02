0419f3c8d4ec03562b81b0ec85636cfdfc0082fe
rob <amesrobert2@gmail.com>
2019-12-02 03:30:48 +0000

    Added very simple swipe thing to go from showlow to cerate log and back in their respective js files

--- Life_Scorer/static/create_log.js
+++ Life_Scorer/static/create_log.js
@@ -149,4 +149,31 @@ function populateForms(taskName){
    vForm.appendChild(submitBtn);
 }
 
+//Implement swipe action
+//Taken from: https://stackoverflow.com/questions/45648886/swipe-left-right-for-a-webpage
+ var start = null;
+ window.addEventListener("touchstart",function(event){
+   if(event.touches.length === 1){
+      //just one finger touched
+      start = event.touches.item(0).clientX;
+    }else{
+      //a second finger hit the screen, abort the touch
+      start = null;
+    }
+  });
 
+ window.addEventListener("touchend",function(event){
+    var offset = 100;//at least 100px are a swipe
+    if(start){
+      //the only finger that hit the screen left it
+      var end = event.changedTouches.item(0).clientX;
+
+      if(end > start + offset){
+              window.location.href="show_log";
+      }
+      if(end < start - offset ){
+              window.location.href="show_log";
+      }
+    }
+  });
+//End swipe implementation
--- Life_Scorer/static/show_log.js
+++ Life_Scorer/static/show_log.js
@@ -34,3 +34,32 @@ function task_text() {
     
     task_text();
   }
+
+//Implement swipe action
+//Taken from: https://stackoverflow.com/questions/45648886/swipe-left-right-for-a-webpage
+ var start = null;
+ window.addEventListener("touchstart",function(event){
+   if(event.touches.length === 1){
+      //just one finger touched
+      start = event.touches.item(0).clientX;
+    }else{
+      //a second finger hit the screen, abort the touch
+      start = null;
+    }
+  });
+
+ window.addEventListener("touchend",function(event){
+    var offset = 100;//at least 100px are a swipe
+    if(start){
+      //the only finger that hit the screen left it
+      var end = event.changedTouches.item(0).clientX;
+
+      if(end > start + offset){
+              window.location.href="create_log";
+      }
+      if(end < start - offset ){
+              window.location.href="create_log";
+      }
+    }
+  });
+//End swipe implementation
