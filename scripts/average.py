84778e0126ab29fbc54a0a525d122af75232a7a3
Rob <amesrobert2@gmail.com>
2019-12-20 22:42:44 +0000

    a little more scripting

--- scripts/average.py
+++ scripts/average.py
@@ -1,8 +1,10 @@
 #average an attribute from db file
-#as written is hardcoded and need to be run from same directory as file
-import sqlite3
-import json
-conn = sqlite3.connect('Rob.db')
+#as written is hardcoded 
+import sqlite3, json, subprocess
+path = subprocess.check_output(['./get_latest_db_path.sh'])
+path =str(path)
+path= path[2:len(path)-3]+'/Rob.db'
+conn = sqlite3.connect(path)
 cur = conn.cursor()
 cur.execute('Select attributes from log where task_id = 14')
 r = cur.fetchall()
---
+++ scripts/get_latest_db_path.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd '/var/www/Life_Scorer/backups/'
+cd "$(ls -rt| tail -n 1)"
+pwd
